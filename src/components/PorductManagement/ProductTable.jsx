01716bcd51dc6727425c0e87323ac4062603272d
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-17 02:31:19 +0600

    product data showing  on table

--- src/components/PorductManagement/ProductTable.jsx
+++ src/components/PorductManagement/ProductTable.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
 const ProductTable = ({product}) => {
+  // console.log(products)
   return (
     <div>
       <h3>Products:{product.length}</h3>
+      <table style={{textAlign:'center'}}>
+        <thead>
+          <tr>
+            <td>No.</td>
+            <td>Name</td>
+            <td>Price</td>
+            <td>Quantity</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            product.map((pro,index) => <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{pro.name}</td>
+              <td>{pro.price}</td>
+              <td>{pro.quantity}</td>
+              <td></td>
+            </tr>)
+          }
+        </tbody>
+      </table>
       
     </div>
   );
