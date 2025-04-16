d7406a078daab3adbee9b31264ce2d9045e5f4da
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-16 22:48:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Explore Form</h1>
+      <SimpleForm></SimpleForm>
+      
+    </>
+  )
+}
+
+export default App
