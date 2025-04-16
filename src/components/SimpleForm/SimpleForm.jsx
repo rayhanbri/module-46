30b8f52672d981ecea6fecf252798ba626d22eac
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-16 23:12:55 +0600

    simple input form and get value by e.targer

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const SimpleForm = () => {
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(e.target.name.value)
+    console.log(e.target.email.value)
+  }
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <input type="text" value="rojoni" name='name'  placeholder='your name'/>
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
